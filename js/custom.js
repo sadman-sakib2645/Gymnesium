7b9c2f2c871d2fa24d62588530d9acae5fc9d965
sadman-sakib2645 <sakibsadman849@gmail.com>
2022-02-14 20:23:15 +0600

    cheaking

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,19 @@ $(document).ready(function(){
         arrows:false ,
         autoplay: true ,
         dots: true,
-        slidesToShow:2
+        slidesToShow:2,
+        responsive:[
+            {
+                breakpoint: 768,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows:false ,   
+                }
+            }
+        ],
     });
 
     new VenoBox({
@@ -37,6 +49,30 @@ $(document).ready(function(){
         centerPadding: '0px',
         prevArrow:'<i class="fas fa-chevron-left leftarrow"></i>',
         nextArrow:'<i class="fas fa-chevron-right rightarrow"></i>',
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrows:false
+                }
+            }
+            
+            
+        ],
+        
     });
 
 });
